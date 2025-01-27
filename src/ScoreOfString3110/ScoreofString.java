fc5221c77dcb5e0552cebfb40dc061a3e4a3d2d6
Joyce Souto <140851285+joycsouto@users.noreply.github.com>
2025-01-27 09:40:44 -0300

    LeetCode Exercises

--- src/ScoreOfString3110/ScoreofString.java
+++ src/ScoreOfString3110/ScoreofString.java
@@ -1,13 +1,30 @@
 package ScoreOfString3110;
 
+
+import static java.lang.Math.abs;
+
 public class ScoreofString {
-    public static void main(String[] args) {
 
-        System.out.println("Hello");
 
+    public int scoreOfString(String s) {
+        int sum = 0;
+        int sub = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char value = s.charAt(i);
+            int cha = (int) value;
+            if(sum != 0 ){
+                sub += abs(sum - cha);
+
+            }
+            sum = cha;
 
         }
+
+        return sub ;
     }
+}
+
 
 
 
